42dedacfb75a13a5422648d19b9069046f274151
Phong Tran <phongtran.kiss1911@gmail.com>
2019-08-24 13:54:03 +0900

    mnu

--- src/data/tools-list.js
+++ src/data/tools-list.js
@@ -4,13 +4,15 @@ const tools = [
     name: 'Generator',
     childs: [
       {
+        // 0-1
         name: 'Strong Password Generator',
-        slug: '/password-generator',
+        slug: '/password-generator/',
         desc: 'random, strong, unique password generator with one click',
       },
       {
+        // 0-2
         name: 'UUID Multi Versions Generator',
-        slug: '/uuid-generator',
+        slug: '/uuid-generator/',
         desc:
           'uuid generator and download, single or multi uuids using v1, v3, v4, v5',
       },
@@ -21,19 +23,24 @@ const tools = [
     name: 'Randomness',
     childs: [
       {
+        // 1-0
         name: 'Random Name Picker',
-        slug: '/random-name-picker',
+        slug: '/random-name-picker/',
         desc: 'pick a random name from list, configurable and shuffleable',
       },
       {
+        // 1-1
         name: 'Yes or No Decision Maker',
-        slug: '/yes-or-no',
-        desc: 'this tool help you make tough decisions (50-50)',
+        slug: '/yes-or-no/',
+        desc:
+          'feeling difficult when making tough decisions? this tool help you make things simpler',
       },
       {
+        // 1-2
         name: 'Random Popular Animals Generator',
-        slug: '/animal-generator',
-        desc: 'generate random animals from most simple and popular list',
+        slug: '/animal-generator/',
+        desc:
+          'generate random animals from most simple and popular list, to play with kids',
       },
     ],
   },
@@ -42,16 +49,24 @@ const tools = [
     name: 'Calculator',
     childs: [
       {
+        // 2-0
         name: 'Google Adsence Calculator',
-        slug: '/google-adsence-calculator',
+        slug: '/google-adsence-calculator/',
         desc:
           'how much traffic and pageviews you need to make a decent living with Google Adsence',
       },
       {
+        // 2-1
         name: 'Multi Calendars Age Calculator',
-        slug: '/age-calculator',
+        slug: '/age-calculator/',
         desc:
           'try to figure out exactly how old you are, including the years, months, and days',
+      },
+      {
+        // 2-2
+        name: 'Date Time Duration Calculator',
+        slug: '/datetime-duration-calculator/',
+        desc: '',
       },
     ],
   },
